71710fa8b509b532e1df6ca343853110b24b44cb
shireesha <shireeshasai.rallabandi@gmail.com>
2018-06-12 11:38:47 +0530

    file

---
+++ arth.py
@@ -0,0 +1,20 @@
+"""
+a module used to perform arthematic operations
+"""
+def addi(first_var,sec_var):
+    """
+    this is a function used to perform addition of two numbers
+    """
+    return first_var+sec_var
+
+def subs(first_var, sec_var):
+    """
+    this is a function used to perform addition of two numbers
+    """
+    return first_var-sec_var
+
+def mul(first_var, sec_var):
+    """
+    this is a function used to perform addition of two numbers
+    """
+    return first_var*sec_var
